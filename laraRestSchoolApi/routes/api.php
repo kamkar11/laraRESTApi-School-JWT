5e5d265bb2cc26d0f60f3b1255b3c0e1474cebf9
Kamil Machanski <kamilmachanski@gmail.com>
2020-06-12 21:50:51 +0200

    create Subject CRUD

--- laraRestSchoolApi/routes/api.php
+++ laraRestSchoolApi/routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::ApiResource('/class','Api\SclassController');
+Route::ApiResource('/subject','Api\SubjectController');
